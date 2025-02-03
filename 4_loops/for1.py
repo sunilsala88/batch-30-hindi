7f03bdcc897d4bc02d4a9de630a6cb254a6333b6
sunilsala88 <sunilsala88@gmail.com>
2025-02-03 13:42:32 +0530

    feb-3

---
+++ 4_loops/for1.py
@@ -0,0 +1,12 @@
+#keywords
+# for, range, print,if ,True,False
+#print,len,input,int,str,float
+list1=[22,344,5,6,44,63]
+s=sum(list1)
+print(s)
+
+total=0
+for i in list1:
+    total=total+i
+
+print(total)
