6a4a1a54a43f029957cfb6bfc2bfcefbc12036da
sunilsala88 <sunilsala88@gmail.com>
2025-02-15 17:46:55 +0530

    feb15-5

--- 5_function/function3.py
+++ 5_function/function3.py
@@ -11,3 +11,55 @@ def fun2(r,q):
     print(d)
 
 fun2(5,10)
+
+
+def order(price=0):
+    if price==0:
+        print('market')
+    else:
+        print(f'limit order with price {price}')
+
+order(100)
+
+#default argument
+def myFun(x, y=50):
+    print("x: ", x)
+    print("y: ", y)
+
+
+# Driver code (We call myFun() with only
+# argument)
+myFun(10,20)
+
+
+#positional arg
+def nameAge(name, age):
+    print("Hi, I am", name)
+    print("My age is ", age)
+
+
+# You will get correct output because 
+# argument is given in order
+print("Case-1:")
+nameAge("Suraj", 27)
+
+print("\nCase-2:")
+nameAge(27, "Suraj")
+
+
+#keyword argument
+# Python program to demonstrate Keyword Arguments
+def student(firstname, lastname):
+    print(firstname, lastname)
+
+
+# Keyword arguments
+student(firstname='Geeks', lastname='Practice')
+student(lastname='Practice', firstname='Geeks')
+
+
+
+def do_somethig():
+    pass
+
+do_somethig()
