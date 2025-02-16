59df40782cd8ba2a7d7c6a274d529f6a9d227d9e
sunilsala88 <sunilsala88@gmail.com>
2025-02-16 17:22:11 +0530

    fb-16-2

--- 6_files_&_excep/file1.py
+++ 6_files_&_excep/file1.py
@@ -1,8 +1,20 @@
 
 
-
+#read txt file 
 f1=open(r'/Users/algo trading 2025/batch 30 hindi/6_files_&_excep/new.txt','r')
 # f1=open(file='new.txt',mode='r')
 content=f1.read()
 print(content)
 f1.close()
+
+a='hello world'
+
+#write to file
+# f2=open(file='data.txt',mode='w')
+# f2.write(a)
+# f2.close()
+
+
+# f3=open(file='data1.txt',mode='a')
+# f3.write('\nthis is third line')
+# f3.close()
---
+++ 6_files_&_excep/file2.py
@@ -0,0 +1,7 @@
+
+
+f1=open('new_data.txt','r')
+data=f1.read()
+print(data)
+
+f1.close()
