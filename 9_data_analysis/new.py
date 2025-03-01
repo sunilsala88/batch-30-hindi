205161744009e12003def277b3040bde67c700c3
sunilsala88 <sunilsala88@gmail.com>
2025-03-01 17:02:49 +0530

    mar-1-1

---
+++ 9_data_analysis/new.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+import sys
+import certifi
+os.environ['SSL_CERT_FILE'] = certifi.where()
+
+
+# Corrected raw URLs for reading CSVs directly
+sp500_url = "https://raw.githubusercontent.com/sunilsala88/batch-29/main/10_data_analysis/sp500.csv"
+stock_value_url = "https://raw.githubusercontent.com/sunilsala88/batch-29/main/10_data_analysis/stock_value.csv"
+sbin_url = "https://raw.githubusercontent.com/sunilsala88/batch-29/main/10_data_analysis/SBIN.csv"
+unicorn_companies_url = "https://raw.githubusercontent.com/sunilsala88/batch-29/main/10_data_analysis/Unicorn_companies.csv"
+
+# Reading CSVs into DataFrames
+sp500_df = pd.read_csv(sp500_url)
+stock_value_df = pd.read_csv(stock_value_url)
+sbin_df = pd.read_csv(sbin_url)
+unicorn_companies_df = pd.read_csv(unicorn_companies_url)
+
+# Display first few rows of one dataset as a sample
+print(sp500_df.head())
---
+++ 9_data_analysis/numpy1.py
@@ -0,0 +1,21 @@
+
+
+# import numpy
+# l1=[33,44,55,6.3]
+# print(l1)
+# a1=numpy.array(l1)
+# print(a1)
+
+
+
+import numpy as np
+l1=[33,44,55,6]
+print(l1)
+a1=np.array(l1)
+print(a1)
+
+print(l1+l1)
+print(a1+a1)
+
+# print(l1-3)
+print(a1%3)
