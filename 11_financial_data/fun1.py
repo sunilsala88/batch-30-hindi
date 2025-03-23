181a499ccbcc0a3b37744ee01032b9fcfed63133
sunilsala88 <sunilsala88@gmail.com>
2025-03-23 17:05:12 +0530

    mar-23-1

--- 11_financial_data/fun1.py
+++ 11_financial_data/fun1.py
@@ -3,10 +3,28 @@
 
 
 import yfinance as yf
-data=yf.download('^NSEBANK')
+data=yf.download('^NSEBANK',start='2021-01-01',end='2021-12-31')
 print(data)
-print(data.info())
-
 data.columns=[i[0] for i in data.columns]
 print(data)
 
+
+data=yf.download('^NSEBANK',period='1mo')
+print(data)
+data.columns=[i[0] for i in data.columns]
+print(data)
+
+
+data=yf.download('^NSEBANK',period='2y',interval='1h')
+print(data)
+data.columns=[i[0] for i in data.columns]
+data.reset_index(inplace=True)
+data['Datetime']=data['Datetime'].dt.tz_convert('Asia/Kolkata')
+print(data)
+
+data=yf.download('^NSEBANK',period='10d',interval='1m')
+print(data)
+data.columns=[i[0] for i in data.columns]
+data.reset_index(inplace=True)
+data['Datetime']=data['Datetime'].dt.tz_convert('Asia/Kolkata')
+print(data)
